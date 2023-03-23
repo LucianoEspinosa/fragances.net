a328edd5bcc62ae154395906eab99d9d0f44dcc6
Luciano Espinosa <lucianoespinosa04@gmail.com>
2023-03-23 08:03:40 -0300

    se agrego funcion para sumar total del carrito y cantidad de producto. Estilizacion del carrito

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function mostrarProductos() {
         tarjetas.innerHTML += `
     
         <div class="d-flex flex-column align-items-center tarjeta">
-        <img src="./img/${item.img}.jpg" class="d-block w-100" alt="${item.nombre}">
+        <img src="./img/${item.img}.jpg" class="d-block w-50" alt="${item.nombre}">
         
             <h2 class="card-title">${item.marca}</h2>
             <h3 class="card-text">${item.nombre}</h3>
@@ -28,9 +28,9 @@ function mostrarProductos() {
         </div>
 `
     }
+
     //  mostrarCarrito()
 }
-
 function agregarAlCarrito(codigoPerfume) {
     // console.log(codigoPerfume);
     let existe = false;
@@ -55,12 +55,11 @@ function agregarAlCarrito(codigoPerfume) {
             }
         }
     }
+
     localStorage.setItem("carritoDeCompras", JSON.stringify(carrito))
+
     mostrarCarrito()
 }
-
-
-
 function eliminarProductosDeCarrito(codigoPerfume) {
     console.log(codigoPerfume);
     let carrito = JSON.parse(localStorage.getItem('carritoDeCompras')) || [];
@@ -75,53 +74,98 @@ function eliminarProductosDeCarrito(codigoPerfume) {
     localStorage.setItem("carritoDeCompras", JSON.stringify(carrito));
     mostrarCarrito();
 }
-function mostrarCarrito() {
-
+function restarUnidades(codigoPerfume) {
+    console.log(codigoPerfume);
     let carrito = JSON.parse(localStorage.getItem('carritoDeCompras')) || [];
-    
+    for (let i = 0; i < carrito.length; i++) {
+        if (carrito[i].codigo == codigoPerfume) {
+            console.log(carrito[i].codigo);
+            carrito[i].cantidad = carrito[i].cantidad - 1;
+            if (carrito[i].cantidad < 1) {
+                console.log("aguante luchito");
+                carrito.splice(i, 1);
+                // eliminarProductosDeCarrito(codigoPerfume);
+            }
+
+            break;
+        }
+    }
+    localStorage.setItem("carritoDeCompras", JSON.stringify(carrito));
+    mostrarCarrito();
+}
+function mostrarCarrito() {
+    let carrito = JSON.parse(localStorage.getItem('carritoDeCompras')) || [];
 
     let itemDecompra = document.getElementById("itemCompra");
-    itemDecompra.innerHTML = ""; // Limpiar contenido previo
-    console.log(carrito);
+    let tablaCarrito = document.getElementById("carritoDropDown");
+    let totalCarrito= document.getElementById("carritoDropDown2");
+    
+    itemDecompra.innerHTML = "";
+    tablaCarrito.innerHTML = "";
+    totalCarrito.innerHTML = "";
+
+
+    
     for (let item of carrito) {
 
         itemDecompra.innerHTML += `
         <div id="div${item.codigo}" class="d-flex itemCarrito justify-content-evenly align-items-center">
-        <img src="../img/${item.img}.jpg" class="imgCarrito" alt="">
+            <img src="../img/${item.img}.jpg" class="imgCarrito" alt="">
 
-        <div class="d-flex flex-column align-items-center justify-content-around ">
-            <span class="">${item.marca + " " + item.nombre + " " + item.presentacion}</span>
-            <div class="unidades d-flex">
-                <i class="bi bi-cart-dash-fill "></i>
-                <input id="cantidad${item.codigo}" type="text" class="cantidad" value="${item.cantidad}">
-                <i class="bi bi-cart-plus-fill " id="sumar${item.codigo}" onclick="agregarAlCarrito(${item.codigo})"></i>
+            <div class="d-flex flex-column align-items-center justify-content-around ">
+                <span class="">${item.marca + " " + item.nombre + " " + item.presentacion}</span>
+                <div class="unidades d-flex">
+                    <i class="bi bi-cart-dash-fill " id="restar${item.codigo}" onclick="restarUnidades(${item.codigo})"></i>
+                    <input id="cantidad${item.codigo}" type="text" class="cantidad" value="${item.cantidad}">
+                    <i class="bi bi-cart-plus-fill " id="sumar${item.codigo}" onclick="agregarAlCarrito(${item.codigo})"></i>
+                </div>
             </div>
-        </div>
 
-        <div class="d-flex flex-column align-items-center justify-content-around ">
-            <span>Precio</span>
+            <div class="d-flex flex-column align-items-center justify-content-around ">
+                <span>Precio</span>
             
-            <div class="unidades" ><span>$${item.precio * item.cantidad}</span></div>
-        </div>
+                <div class="unidades" ><span>$${item.precio * item.cantidad}</span></div>
+            </div>
     
-    <i id="eliminar${item.codigo}" onclick="eliminarProductosDeCarrito(${item.codigo})"
-        class="bi bi-trash3-fill"></i>
-    </div>
-      
-    `
-        //let botonborrar = document.querySelector(`#eliminar${item.codigo}`);
-        // console.log(botonborrar.id);
-        // document.querySelector(`#eliminar${item.codigo}`).addEventListener("click", () => {
-        //     console.log("hizo click en borrar");
-        //     eliminarProductosDeCarrito(item.codigo);
-        //     // console.log(item.codigo);
-        //     document.querySelector(`#div${item.codigo}`).remove();
+            <i id="eliminar${item.codigo}" onclick="eliminarProductosDeCarrito(${item.codigo})"
+            class="bi bi-trash3-fill"></i>
+        </div>`
 
-        // })
-
+        tablaCarrito.innerHTML += `
+    <tr class="text-center">
+        <td class=""><img class="tableImg" src="../img/${item.img}.jpg"</td>
+        <td>${item.marca}</td>
+        <td>${item.nombre}</td>
+        <td>${item.presentacion}</td>
+        <td>${item.cantidad}</td>
+        <td>$${item.precio}</td>
+        <td><i id="eliminar${item.codigo}" onclick="eliminarProductosDeCarrito(${item.codigo})"
+        class="bi bi-trash3-fill vaciarCarrito"></i></td>
+    </tr>`
     }
-
+    let precio=localStorage.getItem("precioTotal");
+    let cantidad=JSON.parse(localStorage.getItem("CantidadProductos"));
+    
+    totalCarrito.innerHTML = `
+    <tr class="text-center">
+        <td colspan="7"><a>Finalizar Compra</a></td>
+        <td>${cantidad}</td>
+        <td>$${precio}</td>
+        <td><i class="bi bi-trash3-fill vaciarCarrito"></i></td>
+    </tr>
+    
+    `
+    MontoTotal()
 }
 
 
+function MontoTotal() {
+    let carrito = JSON.parse(localStorage.getItem('carritoDeCompras')) || [];
+    let precio = carrito.reduce((total, i) => total + i.precio * i.cantidad, 0);
 
+    let cantidad = carrito.reduce((cantidad, item) => cantidad + item.cantidad, 0);
+
+
+    localStorage.setItem("cantidadProductos", (cantidad));
+    localStorage.setItem("precioTotal", (precio));
+}
